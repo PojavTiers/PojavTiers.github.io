b12f128d5af6b4a212e1146261e9a7a86855b966
PojavTiers <sa6648495@gmail.com>
2026-01-19 15:31:50 +0530

    Create players.js

---
+++ players.js
@@ -0,0 +1,7 @@
+const players = [
+  { name:"Player1", tier:"Combat Grandmaster", rank:1, medal:"gold" },
+  { name:"Player2", tier:"Combat Master", rank:2, medal:"silver" },
+  { name:"Player3", tier:"Combat Master", rank:3, medal:"bronze" },
+  { name:"Player4", tier:"Combat Elite", rank:4 },
+  { name:"Player5", tier:"Combat Elite", rank:5 }
+];
